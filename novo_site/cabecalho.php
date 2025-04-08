88e00f41ae950a38c0cd8d90c791b5efb1c2b8e5
Eduardo Pelloso <psantanaeduardo@gmail.com>
2025-04-08 16:49:47 -0300

    1.2
    Incluída página de explicação sobre automação dos comandos da portaria e mais alguns ajustes. Também foi adicionado e padronizado o footer.

---
+++ novo_site/cabecalho.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Systech - Automação Predial</title>
+    <link rel="icon" type="image/x-icon" href="image/Systechnovo.png">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">   
+
+    <header>
+    <div class="d-flex align-items-center">
+            <div id="logo" class="me-4 d-flex gap-3">
+                <img style="max-height: 40px" class="img-fluid" src="imagens/LOGO_BRANCO1000.png" alt="">
+            </div>
+            
+            <div class="d-flex gap-3" id="botoesheader">
+                <a class="text-decoration-none text-light fs-5" href="index.html">Home</a>
+                <a class="text-decoration-none text-light fs-5" href="">Obras</a>
+                <a class="text-decoration-none text-light fs-5" href="">Contato</a>
+            </div>
+        </div>
+    </header>
